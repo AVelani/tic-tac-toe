873ec9b90a61f6bdb8333b6016a2e18f797dc347
AVelani <aniketvelani.av@gmail.com>
2023-05-08 19:24:44 +0530

    App.js modified added calculateWinner(square) function

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import Square from "./Square";
 
 const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
     const handleClick = (i) => {
+        if (calculatorWinner(squares) || squares[i]) return;
         const nextSquare = squares.slice();
-        nextSquare[i] = "X";
+        if (xIsNext) {
+            nextSquare[i] = "X";
+        } else {
+            nextSquare[i] = "O";
+        }
         setSquares(nextSquare);
+        setXIsNext(!xIsNext);
     };
     useEffect(() => {
         console.log(squares);
     }, [squares]);
+
+    const winner = calculatorWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player " + (xIsNext ? "X" : "O");
+    }
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square
                     value={squares[0]}
@@ -68,3 +84,25 @@ const App = () => {
 };
 
 export default App;
+function calculatorWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[b] === squares[c]
+        )
+            return squares[a];
+    }
+    return null;
+}
